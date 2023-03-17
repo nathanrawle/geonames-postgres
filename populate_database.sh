85cc2488524069ef59aeed2c5f15ef81c2e2185f
Nathan Rawle <nathan.rawle@gmail.com>
2023-03-17 15:47:38 +0000

    feat: read source directly into psql copy

--- populate_database.sh
+++ populate_database.sh
@@ -4,53 +4,5 @@ PSQL="psql -X --username=db_maker --dbname=geonames -t --no-align -c"
 
 $PSQL "TRUNCATE TABLE geonames"
 
-sed -E 's/\t{2}/\tNULL\t/g' ./cities500.txt \
-| sed -E 's/\t{2}/\tNULL\t/g' \
-| sed -E 's/\x27/\x27\x27/g' \
-| while IFS=$'\t' read GID NAME ASCII ALT LAT LON FEAT_CLASS FEAT_CODE COUNTRY_CODE ALT_CC ADMIN1 ADMIN2 ADMIN3 ADMIN4 POP ELEVATION DEM TIMEZONE MOD_DATE
-do
-  INSERT_RESULT=$($PSQL "INSERT INTO geonames (
-    geoname_id, 
-    geoname,
-    asciiname,
-    latitude,
-    longitude,
-    feature_class,
-    feature_code,
-    country_code,
-    alt_country_code,
-    admin1_code,
-    admin2_code,
-    admin3_code,
-    admin4_code,
-    population,
-    elevation,
-    dem,
-    timezone,
-    gn_modification_date
-  )
-  VALUES (
-    $GID,
-    NULLIF('$NAME','NULL'),
-    NULLIF('$ASCII','NULL'),
-    $LAT,
-    $LON,
-    NULLIF('$FEAT_CLASS','NULL'),
-    NULLIF('$FEAT_CODE','NULL'),
-    NULLIF('$COUNTRY_CODE','NULL'),
-    NULLIF('$ALT_CC','NULL'),
-    NULLIF('$ADMIN1','NULL'),
-    NULLIF('$ADMIN2','NULL'),
-    NULLIF('$ADMIN3','NULL'),
-    NULLIF('$ADMIN4','NULL'),
-    $POP,
-    $ELEVATION,
-    $DEM,
-    NULLIF('$TIMEZONE','NULL'),
-    NULLIF('$MOD_DATE','NULL')::date
-  )")
-  if [[ $INSERT_RESULT != 'INSERT 0 1' ]]
-  then
-    echo "INSERT failed for $GID $NAME"
-  fi
-done
+awk -F $'\t' '{ print $1 "\t" $2 "\t" $3 "\t" $5 "\t" $6 "\t" $7 "\t" $8 "\t" $9 "\t" $10 "\t" $11 "\t" $12 "\t" $13 "\t" $14 "\t" $15 "\t" $16 "\t" $17 "\t" $18 "\t" $19 }' cities500_test.txt \
+| $PSQL "\copy geonames FROM STDIN DELIMITER E'\t' CSV"

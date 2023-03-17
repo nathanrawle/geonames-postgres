09a8d18283b59cbef66045b195e695cd43af0b40
Nathan Rawle <nathan.rawle@gmail.com>
2023-03-17 19:14:01 +0000

    feat: copy feature classes into database

--- populate_database.sh
+++ populate_database.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+DDIR_ABS="$HOME/projects/data/sourced/geonames"
 PSQL="psql -X --username=db_maker --dbname=geonames -t --no-align -c"
 
-$PSQL "TRUNCATE TABLE geonames"
+$PSQL "TRUNCATE TABLE geonames, feature_classes"
+
+$PSQL "\copy feature_classes FROM $DDIR_ABS/feature_classes.txt DELIMITER E'\t' CSV"
 
 awk -F $'\t' '{ print $1 "\t" $2 "\t" $3 "\t" $5 "\t" $6 "\t" $7 "\t" $8 "\t" $9 "\t" $10 "\t" $11 "\t" $12 "\t" $13 "\t" $14 "\t" $15 "\t" $16 "\t" $17 "\t" $18 "\t" $19 }' cities500_test.txt \
 | $PSQL "\copy geonames FROM STDIN DELIMITER E'\t' CSV"

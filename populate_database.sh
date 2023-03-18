dd20954d239ff9b51f561c20b2162e50da7d8bd6
Nathan Rawle <nathan.rawle@gmail.com>
2023-03-18 01:32:47 +0000

    feat: populate admin2_codes table

--- populate_database.sh
+++ populate_database.sh
@@ -3,7 +3,7 @@
 DDIR_ABS="$HOME/projects/data/sourced/geonames"
 PSQL="psql -X --username=db_maker --dbname=geonames -t --no-align -c"
 
-$PSQL "TRUNCATE TABLE geonames, feature_classes, feature_codes, country_info, admin1_codes"
+$PSQL "TRUNCATE TABLE geonames, feature_classes, feature_codes, country_info, admin1_codes, admin2_codes"
 
 $PSQL "\copy feature_classes FROM $DDIR_ABS/feature_classes.txt DELIMITER E'\t' CSV"
 
@@ -15,3 +15,5 @@ sed -E 's/^([A-Z]{2})\./\1\t/' admin1CodesASCII.txt | $PSQL "\copy admin1_codes 
 
 awk -F $'\t' '{ print $1 "\t" $2 "\t" $3 "\t" $5 "\t" $6 "\t" $7 "\t" $8 "\t" $9 "\t" $10 "\t" $11 "\t" $12 "\t" $13 "\t" $14 "\t" $15 "\t" $16 "\t" $17 "\t" $18 "\t" $19 }' cities500_test.txt \
 | $PSQL "\copy geonames FROM STDIN DELIMITER E'\t' CSV"
+
+sed -E 's/^([A-Z]{2})\.([A-Z0-9]+)\./\1\t\2\t/' admin2Codes.txt | $PSQL "\copy admin2_codes FROM STDIN DELIMITER E'\t' CSV QUOTE E'\b'"
